bc826bb27f05120010ec4053322429f79a15108c
Hung-God <83905082+Hung-God@users.noreply.github.com>
2021-05-15 11:20:00 +0700

    Revert "Revert "Update Source.cpp""
    This reverts commit fc019e9a078cc7ea99f1178f51dbe1cfa65b3ddd.

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int demsotutrongchuoi(char chuoi[])
+{
+	int sotu = 0, i = 0;
 	int n = strlen(chuoi);
+	do
 	{
+		if (chuoi[i] == ' ' || chuoi[i] == ',' || chuoi[i] == '.')
+		{
 			i++;
+			continue;
+		}
 		sotu++;
+		while (chuoi[i] != ' ' && chuoi[i] != ',' && chuoi[i] != '.' && chuoi[i] != '\0')
+		{
+			i++;
+		}
+	} while (i < n);
+	return sotu;
+}
+
+int main()
+{
+	char chuoi[100];
+	printf("Nhap chuoi: "); gets_s(chuoi);
+	printf("%d", demsotutrongchuoi(chuoi));
+}

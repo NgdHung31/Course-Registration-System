d8f3a670367ea9622196f4ad75c809ffc6c83062
kyphan308 <86183359+kyphan308@users.noreply.github.com>
2021-07-02 09:39:31 +0700

    Add files via upload
    Register + Check Loggin

---
+++ Source.cpp
@@ -0,0 +1,35 @@
+﻿#include "Header.h"
+
+
+void register_a_account()
+{
+	string username, password;
+
+	cout << "Sign up a username - (username should be a ID Student Number) : ";
+	cin >> username;
+	cout << "Sign up a password: ";
+	cin >> password;
+
+	ofstream f;
+	f.open("D:\\" + username + "txt");
+	f << username << endl << password;
+	f.close();
+}
+
+bool check_loggin(string username, string password)
+{
+	string ckuser, ckpassword;
+
+	ifstream f("D:\\" + username + ".txt");
+	getline(f, ckuser);
+	getline(f, ckpassword);
+
+	if ((ckuser == username) && (ckpassword == password))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
